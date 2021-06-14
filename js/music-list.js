bfd53475ac7956b6d62501da1fc51e9d53634c47
nikitak-24 <nikitakadambala@gmail.com>
2021-06-14 19:29:47 +0530

    Music Player

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name: "Charlie Puth - Attention",
+        artist: "Charlie Puth",
+        img: "music-1.jpg",
+        src: "Attention"
+    },
+    {
+        name: "Linkin Park - In The End",
+        artist: "Linkin Park",
+        img: "music-2.jpg",
+        src: "InTheEnd"
+    },
+    {
+        name: "Justin Bieber - Intentions",
+        artist: "Justin Bieber",
+        img: "music-3.jpg",
+        src: "intentions"
+    },
+    {
+        name: "Maroon 5 - Memories",
+        artist: "Maroon 5",
+        img: "music-4.jpg",
+        src: "Memories"
+    },
+    {
+        name: "Camilo Cabello - Senorita ",
+        artist: "Camilo Cabello, Shawn Mendes",
+        img: "music-5.jpg",
+        src: "senorita"
+    },
+    {
+        name: "Imagine Dragons - Thunder",
+        artist: "Imagine Dragons",
+        img: "music-6.jpg",
+        src: "thunder"
+    },
+]
